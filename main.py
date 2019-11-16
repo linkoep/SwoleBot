ad2bd7f65b82749b8c578863005cb0d4d9b4210c
Matthew Brown <brownm18@rpi.edu>
2019-11-16 15:08:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,28 @@ import requests, json, os
 # returns unused except for testing
 
 def AddingEvent(request):
-    bot_id = os.getenv('BOT_ID')
-    
-    request_dict = request.get_json()
-    
-    if request_dict["sender_type"] != 'bot':
-        if len(request_dict["attachments"]) != 0:
-            return f'BBB'
-        else:
-            data = json.dumps({"text" : "{} sent by {} of type {}".format(request_dict["text"], request_dict["sender_id"], request_dict["sender_type"]), "bot_id": bot_id})
-            send = requests.post("https://api.groupme.com/v3/bots/post", data=data)
-            return f'AAA'
-    else:
-        return f'Hello World!'
+	bot_id = os.getenv('BOT_ID')
+	
+	request_dict = request.get_json()
+	
+	if request_dict["sender_type"] != 'bot':
+		if len(request_dict["attachments"]) != 0:
+
+			names = []
+			i = 0;
+			while (request_dict["text"].find('@', i)):
+				j = request_dict["text"].find('@', i+1)
+
+				if j != -1:
+					names.append(request_dict[i:j-1])
+				else:
+					names.append(request_dict[i:len(request_dict)])
+
+
+			statement = ""
+			for temp in names:
+				statement += temp + " ";
+
+			# statement = "{} sent by {} of type {}".format(request_dict["text"], request_dict["sender_id"], request_dict["sender_type"])
+			data = json.dumps({"text": statement, "bot_id": bot_id})
+			requests.post("https://api.groupme.com/v3/bots/post", data=data)
