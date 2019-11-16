4b2cd1175cf6065144d35eadf7fc7f89f6e4c32d
Matthew Brown <brownm18@rpi.edu>
2019-11-16 16:23:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import requests, json, os
 def sendMessage(message):
 	bot_id = os.getenv('BOT_ID')
 	data = json.dumps({"text": message, "bot_id": bot_id})
-    requests.post("https://api.groupme.com/v3/bots/post", data=data)
+	requests.post("https://api.groupme.com/v3/bots/post", data=data)
 
 def AddingEvent(request):
 	# Parse input and avoid self-replies
 	request_dict = request.get_json()
 	if request_dict["sender_type"] == 'bot':
-            return 'Bot message. Do not reply'
-        sendMessage(json.dumps(request_dict))
+		return 'Bot message. Do not reply'
+	sendMessage(json.dumps(request_dict))
 
-        # Respond to workout photos
-        if len(request_dict["attachments"]) != 0:
+	# Respond to workout photos
+	if len(request_dict["attachments"]) != 0:
+		return 'GHGHGHGH'
