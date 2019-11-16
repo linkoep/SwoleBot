e8eadc03b923c5dc6c8d42f1c82d637531c09b2f
Matthew Brown <brownm18@rpi.edu>
2019-11-16 17:57:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests, json, os
 from google.cloud import firestore
 
-core = ["core", "abs"]
-upper = ["upper"]
-lower = ["lower", "leg", "legs"]
-cardio =["cardio", "run", "ran"]
-skills = ["skills", "threw"]
+core = ["core", "abs", "plank"]
+upper = ["upper", "chest", "back", "shoulder", "tricep", "tri", "bicep", "push up"]
+lower = ["lower", "leg", "squat"]
+cardio =["cardio", "run", "ran", "swim", "swam", "pool", "canaan", "ultisquash", "bike", "bicycle"]
+skills = ["skills", "throw", "threw", "canaan", "ultisquash", "ultimate", "frisbee"]
+recovery = ["recovery", "kit", "ice", "rest", "heat", "stretch"]
 
 def sendMessage(message):
 	bot_id = os.getenv("BOT_ID")
@@ -24,22 +25,38 @@ def addWorkout(msg_id, workout_type, unix_time, list_ids):
 		})
 	
 def WorkOutType(message):
+
+	workouts = []
+
 	for word in core:
 		if message.find(word) != -1:
-			return "core"
+			workouts.append("core")
+			break
 	for word in upper:
 		if message.find(word) != -1:
-			return "upper"
+			workouts.append("upper")
+			break
 	for word in lower:
 		if message.find(word) != -1:
-			return "lower"
+			workouts.append("lower")
+			break
 	for word in cardio:
 		if message.find(word) != -1:
-			return "cardio"
+			workouts.append("cardio")
+			break
 	for word in skills:
 		if message.find(word) != -1:
-			return "skills"
-	return "unkown"
+			workouts.append("skills")
+			break
+	for word in recovery:
+		if message.find(word) != -1:
+			workouts.append("recovery")
+			break
+	if len(workouts) == 0:
+		workouts.append("unkown")
+
+	return workouts
+
 
 def AddingEvent(request):
 	# Parse input and avoid self-replies
@@ -60,5 +77,7 @@ def AddingEvent(request):
 
 	if imageFound:
 		typeOfWorkout = WorkOutType(request_dict["text"].lower())
-		addWorkout(request_dict["id"], typeOfWorkout, request_dict["created_at"], names)
-		sendMessage("Logged a {} workout from {}!".format(typeOfWorkout, request_dict["name"]))
+
+		for temp in typeOfWorkout:
+			addWorkout(request_dict["id"], temp, request_dict["created_at"], names)
+			sendMessage("Logged a {} workout from {}!".format(temp, request_dict["name"]))
