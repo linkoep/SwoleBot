50554417ef4ea0a0e4068a5d076cf944139b1cfb
Matthew Brown <brownm18@rpi.edu>
2019-11-16 15:37:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ def AddingEvent(request):
 	if request_dict["sender_type"] != 'bot':
 		if len(request_dict["attachments"]) != 0:
 
-			statement = "AAA"
-
 			names = []
 			i = 0;
 			while (request_dict["text"].find('@', i) != -1):
@@ -20,20 +18,15 @@ def AddingEvent(request):
 				if j != -1:
 					names.append(request_dict[i:j-2])
 				else:
-					names.append(request_dict[i:len(request_dict)])
+					names.append(request_dict[i:])
 
-				statement += "i: " + i + " j; " + j
-				data = json.dumps({"text": statement, "bot_id": bot_id})
-				requests.post("https://api.groupme.com/v3/bots/post", data=data)
-				
 				i = j
 
+			statement = " "
 			for temp in names:
 				statement += temp + " ";
 
 			statement += " " + request_dict["text"];
-
-			# statement = "{} sent by {} of type {}".format(request_dict["text"], request_dict["sender_id"], request_dict["sender_type"])
 	
 			data = json.dumps({"text": statement, "bot_id": bot_id})
 			requests.post("https://api.groupme.com/v3/bots/post", data=data)
