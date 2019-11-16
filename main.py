e30e644b4cf4c3cdeb398e5190709a677f3c1433
Linus Koepfer <linkoep@gmail.com>
2019-11-16 16:11:17 -0500

    Refactor

--- main.py
+++ main.py
@@ -2,35 +2,43 @@ import requests, json, os
 
 # returns unused except for testing
 
-def AddingEvent(request):
+def sendMessage(message):
 	bot_id = os.getenv('BOT_ID')
-	
+        data = json.dumps({"text": message, "bot_id": bot_id})
+        requests.post("https://api.groupme.com/v3/bots/post", data=data)
+
+def AddingEvent(request):
+        # Parse input and avoid self-replies
 	request_dict = request.get_json()
-	
-	if request_dict["sender_type"] != 'bot':
-		if len(request_dict["attachments"]) != 0:
+	if request_dict["sender_type"] == 'bot':
+            return 'Bot message. Do not reply'
+        sendMessage(json.dumps(request_dict))
 
-			statement = " "
+        # Respond to workout photos
+        if len(request_dict["attachments"]) != 0:
+            
 
-			names = []
-			i = 0;
-
-			while (request_dict["text"].find('@', i) != -1):
-				j = request_dict["text"].find('@', i+1)
-
-				if j != -1:
-					names.append(request_dict["text"][i:j-2])
-				else:
-					names.append(request_dict["text"][i:])
-				i = j
-
-			
-			for temp in names:
-				statement += str(temp) + " "
-
-			statement += len(names)
-
-			statement += " " + request_dict["text"]
-	
-			data = json.dumps({"text": statement, "bot_id": bot_id})
-			requests.post("https://api.groupme.com/v3/bots/post", data=data)
+                #statement = ""
+#
+                #names = []
+                #i = 0;
+#
+                ## statement += str(request_dict["text"].find("@", i))
+#
+                #while (request_dict["text"].find('@', i) != -1):
+                        #j = request_dict["text"].find('@', i+1)
+#
+                        #if j != -1:
+                                #names.append(request_dict[i:j-2])
+                        #else:
+                                #names.append(request_dict[i:])
+#
+                        #i = j
+#
+                #
+                #for temp in names:
+                        #statement += str(temp) + " "
+#
+                #statement += " " + request_dict["text"]
+                #
+                #sendMessage(statement)
