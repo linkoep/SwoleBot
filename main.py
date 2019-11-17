6710991c20e92c1538482fb2c1d6c3cf22790e1b
Linus Koepfer <linkoep@gmail.com>
2019-11-17 04:56:28 -0500

    debug cron

--- main.py
+++ main.py
@@ -179,7 +179,8 @@ def AddingEvent(request):
 	# Parse input and avoid self-replies
 	request_dict = request.get_json()
 	if not request_dict:
-		sendMessage("Ahhh")
+		print("Forcing JSON Conversion", file=sys.stderr)
+		request_dict = request.get_json(force=True)
 	if request_dict["sender_type"] == "bot":
 		return "Bot message. Do not reply"
 	
