36b6bb6ad3f7229a08ae905147bb00b68ff7e66b
Matthew Brown <brownm18@rpi.edu>
2019-11-17 00:52:50 -0500

    commit

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import datetime
 import pickle
 import os.path
 from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 import requests, json, os
 from google.cloud import firestore
@@ -78,26 +80,19 @@ def FindEvents():
 
 	creds = None
 	if os.path.exists('token.pickle'):
-		sendMessage("AAAA")
-		credentials.json
-
-		with open('token.pickle', 'rb') as token:
-			creds = pickle.load(token)
-
-	if os.path.exists('credentials.json'):
-		sendMessage("BBB")
-		with open('credentials.json', 'rb') as token:
-			creds = pickle.load(token)
-
-	if os.path.exists('credentials'):
-		sendMessage("CCC")
-		with open('credentials', 'rb') as token:
-			creds = pickle.load(token)
-
-	if creds == None:
-		sendMessage("BADDD")
-		return "BADDD"
-	sendMessage("Good: 3")
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
 	service = build('calendar', 'v3', credentials=creds)
 
