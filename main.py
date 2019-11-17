5a1b5e627e186a7f3b2cee1f436505dcb10a5631
Matthew Brown <brownm18@rpi.edu>
2019-11-17 07:34:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -116,23 +116,9 @@ def getKitHours():
 	service = build("calendar", "v3", credentials=creds)
 
 	# Call the Calendar API
-
-	"""
-	now = datetime.now()
-	temp = now - timedelta(hours=5)
-	sendMessage(temp.isoformat())
-
-	events_result = service.events().list(calendarId="jqurd415p17322i9p9sqmq5g78@group.calendar.google.com", 
-		timeMin=datetime.now(), maxResults=10, orderBy="startTime").execute()
-	"""
-
 	now = datetime.utcnow().isoformat() + "Z" # "Z" indicates UTC time
 	events_result = service.events().list(calendarId="jqurd415p17322i9p9sqmq5g78@group.calendar.google.com", 
 		timeMin=now, maxResults=4, singleEvents=True, orderBy="startTime").execute()
-
-
-
-
 
 	events = events_result.get("items", [])
 
@@ -143,12 +129,7 @@ def getKitHours():
 		for event in events:
 			start = str(event["start"].get("dateTime", event["start"].get("date")))
 			end = str(event["end"].get("dateTime", event["end"].get("date")))
-			# temp = start, event["summary"]
-			# print(start, event["summary"])
-			# statement += "\n" + start
-
 			statement += "\nOn {}/{}/{} @ {}:{} - {}:{}".format(start[0:4], start[5:7], start[8:10], start[11:13], start[14:16], end[11:13], end[14:16])
-
 	return statement
 
 def setKitHours(message):
@@ -159,11 +140,8 @@ def setKitHours(message):
 			creds = pickle.load(token)
 	service = build("calendar", "v3", credentials=creds)
 
-	# d.isoformat()
-
 	# set Kit's Hours 11/18/2019 09:30-14:30
 
-	"""
 	i = message.find("/")
 	if i == -1:
 		return "Invalid Input"
@@ -187,7 +165,9 @@ def setKitHours(message):
 	endMin = int(message[k+1:k+3])
 
 	start = datetime(year, month, day, startHour, startMin, 0, 0)
+	sendMessage(start.isoformat())
 	end = datetime(year, month, day, endHour, endMin, 0, 0)
+	sendMessage(end.isoformat())
 	
 	event = {
 		"summary": "Kit's Hours",
@@ -217,6 +197,7 @@ def setKitHours(message):
 			"timeZone": "America/New_York",
 		},
 	}
+	"""
 
 	event = service.events().insert(calendarId="jqurd415p17322i9p9sqmq5g78@group.calendar.google.com", body=event).execute()
 
