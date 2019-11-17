c554383d00cc82a081e6e4fc246a0f867411e27b
Linus Koepfer <linkoep@gmail.com>
2019-11-16 23:51:33 -0500

    Add name helper

---
+++ NameHelper/main.py
@@ -0,0 +1,14 @@
+import requests, json, os
+from google.cloud import firestore
+
+
+def populateNames(request):
+	debug = os.getenv("DEBUG", "false")
+	access_token = os.getenv("ACCESS_TOKEN")
+	group_id = os.getenv("GROUP_ID")
+		
+	if debug.lower() == "true":
+		print("Fetching last 50 messages")
+	messages = requests.get("https://api.groupme.com/v3/groups/"+group_id+"/messages?token="+access_token+"&limit=50")
+	print(messages)
+	return messages
