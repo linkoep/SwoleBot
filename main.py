8823870e533bf679357b36c047e7243d8696be2a
Matthew Brown <brownm18@rpi.edu>
2019-11-17 00:09:29 -0500

    commit

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import requests, json, os
 from google.cloud import firestore
 
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
 core = ["core", "abs", "plank"]
 upper = ["upper", "chest", "back", "shoulder", "tricep", "tri", "bicep", "push up"]
 lower = ["lower", "leg", "squat"]
@@ -66,6 +75,59 @@ def getLeaderboardTop(n):
 	for person in top:
 		sendMessage("{} => {} ".format(person.id, json.dumps(person.to_dict())))
 
+def FindEvents():
+	db = firestore.Client()
+	events = []
+	# top = db.collection("users").order_by("num_workouts", direction=firestore.Query.DESCENDING).limit(n).stream()
+
+	statement = "Events: "
+
+	for event in events:
+		statement += event + "\n"
+
+	sendMessage(statement)
+
+
+def main():
+	"""Shows basic usage of the Google Calendar API.
+	Prints the start and name of the next 10 events on the user's calendar.
+	"""
+	creds = None
+	# The file token.pickle stores the user's access and refresh tokens, and is
+	# created automatically when the authorization flow completes for the first
+	# time.
+	if os.path.exists('token.pickle'):
+		with open('token.pickle', 'rb') as token:
+			creds = pickle.load(token)
+	# If there are no (valid) credentials available, let the user log in.
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file(
+				'credentials.json', SCOPES)
+			creds = flow.run_local_server(port=0)
+		# Save the credentials for the next run
+		with open('token.pickle', 'wb') as token:
+			pickle.dump(creds, token)
+
+	service = build('calendar', 'v3', credentials=creds)
+
+	# Call the Calendar API
+	now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+	print('Getting the upcoming 10 events')
+	events_result = service.events().list(calendarId='primary', timeMin=now,
+										maxResults=10, singleEvents=True,
+										orderBy='startTime').execute()
+	events = events_result.get('items', [])
+
+	if not events:
+		print('No upcoming events found.')
+	for event in events:
+		start = event['start'].get('dateTime', event['start'].get('date'))
+		print(start, event['summary'])
+
+
 def AddingEvent(request):
 	# Parse input and avoid self-replies
 	request_dict = request.get_json()
@@ -79,8 +141,11 @@ def AddingEvent(request):
 	if message.startswith('!bot '):
 		message = message[5:]
 		if message.startswith('leaderboard'):
-			sendMessage("doing leaderboard")
+			sendMessage("Calculating Leaderboard. Please Wait a Second...")
 			getLeaderboardTop(5)
+		elif message.startswith('events'):
+			sendMessage("Finding Events. Please Wait a Second...")
+			FindEvents()
 
 	# Non bot-commands
 	else: 
